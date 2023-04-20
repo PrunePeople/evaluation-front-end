0f33947930db5d878308e7f11690d52b0c5de62f
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-04-20 23:02:23 +0200

    voir correction.md

--- EXO7_DEDIEU_AUDREY/script.js
+++ EXO7_DEDIEU_AUDREY/script.js
@@ -7,7 +7,7 @@ var bgImg = document.querySelector('.bg-img');
 // On définit une fonction qui va changer l'image de fond aléatoirement
 function changeBackgroundImage() {
   var randomIndex = Math.floor(Math.random() * images.length);   // On choisit un nombre aléatoire entre 0 et la longueur de notre tableau d'images
-  bgImg.style.backgroundImage = 'url(/EXO7_DEDIEU_AUDREY/src/img/' + images[randomIndex] + ')';   // On définit l'image de fond de la div avec l'image aléatoire choisie
+  bgImg.style.backgroundImage = 'url(./src/img/' + images[randomIndex] + ')';   // On définit l'image de fond de la div avec l'image aléatoire choisie
 }
 
 // On appelle la fonction pour changer l'image de fond lors du chargement de la page
