ca989283fb4b3114783ff4881b763f6c960b6e51
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-04-20 21:55:07 +0200

    voir correction.md

---
+++ EXO4_DEDIEU_AUDREY/correction.md
@@ -0,0 +1,6 @@
+#### Mes corrections
+
+J'essaye juste de contribuer.
+Tellement propre comme boulot.
+
+>J'ai retirer les commentaire du script et ajouter attibut defer sur la balise script html
--- EXO5_DEDIEU_AUDREY/script.js
+++ EXO5_DEDIEU_AUDREY/script.js
@@ -1,24 +1,24 @@
 // récupérer les éléments du DOM
+
 const passwordInput = document.querySelector('#password');
-const showPasswordBtn = document.querySelector('#show-password');
-const clearBtn = document.querySelector('#effacer-btn');
+const showPasswordImg = document.querySelector('#show-password');
 const contactForm = document.querySelector('#contact-form');
+//Stockage des chemin des images dans un tableau
+const eyePathImgArray = ["./src/img/eye_close.png", "./src/img/eye_open.png"];
+
+
 
 // fonction pour afficher/masquer le mot de passe
 function togglePassword() {
-  if (passwordInput.type === 'password') { /* Test si le type de l'input est de type password (par défaut: masqué) */
-    passwordInput.type = 'text'; /* Change le type de l'input en text (par défaut, il devient donc visible) */
-    showPasswordBtn.textContent = 'Masquer'; /* Change le texte du DOM HTML correspondant en "Masquer" */
+  if (passwordInput.type === 'password') { 
+    passwordInput.type = 'text';
+    showPasswordImg.src = eyePathImgArray[1];
   } else {
-    passwordInput.type = 'password'; /* Sinon le type de l'input est en password */
-    showPasswordBtn.textContent = 'Afficher'; /* Et le texte du DOM HTML en "Afficher" */
+    passwordInput.type = 'password';
+    showPasswordImg.src = eyePathImgArray[0];
   }
 }
 
 // ajouter un événement au clic sur le bouton "Afficher/Masquer"
-showPasswordBtn.addEventListener('click', togglePassword);
+showPasswordImg.addEventListener('click', togglePassword);
 
-// ajouter un événement au clic sur le bouton "Effacer"
-clearBtn.addEventListener('click', function() {
-  contactForm.reset();
-});
